fc7885d14e80a554a6659a6dff669a53ce3165aa
Audarya07 <audiuttarwar2000@gmail.com>
2021-06-27 11:49:16 +0530

    Start THA-10

--- Frontend/THA_10/index.js
+++ Frontend/THA_10/index.js
@@ -1,21 +1,44 @@
-let images = ["./images/guido.jpeg", "./images/Python.png"]
+// let images = ["./images/guido.jpeg", "./images/dennis.jpeg", "./images/bjarnes.jpeg", "./images/james.jpeg", "./images/brendan.jpeg", "./images/yukihiro.jpeg", "./images/larry.jpeg", "./images/rasmus.jpeg"]
 
-let pair = {
-    "./images/guido.jpeg" : "./images/Python.png"
-} 
+
+let images = ["Audi", "Pranjal", "Rajat", "Venkya", "Rohan", "Kunal", "Saurabh", "Tanmay"]
+
+images = [...images, ...images]
 
 for(let i = 0; i < 16; i++){
     let card = document.createElement("div");
     card.classList.add("card");
-    let cards = document.getElementById("cards");
-    cards.appendChild(card);
+
+    let front = document.createElement("div");
+    front.classList.add("front");
+
+    let back = document.createElement("div");
+
+    let rand = Math.floor(Math.random()*(16-i));
+    // back.innerHTML = "<img src='"+images[rand]+"'/>"
+    back.innerHTML = "<h2>" + images[rand] + "</h2>"
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    document.getElementById("cards").appendChild(card);
+    back.classList.add("back");
 }
 
+let clicked;
 let chances = 28;
-let cards = document.querySelectorAll(".card");
+let cards = document.getElementsByClassName("card");
 
-for(let i = 0; i < 16; i++){
-    cards[i].addEventListener("click", (b) => {
-        
+for(let card of cards){
+    card.addEventListener("click", (b) => {
+        chances--;
+        document.querySelector("span").innerHTML = chances;
+        if(clicked){
+            setTimeout(() => {
+                if(card.lastChild.innerHTML != clicked.lastChild.innerHTML){
+
+                }
+            }, 500);
+        }
     })
 }
