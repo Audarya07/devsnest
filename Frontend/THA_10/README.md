7c63947c2a48b3021b2e297bf17f7b6f9fcf5b0e
Audarya07 <audiuttarwar2000@gmail.com>
2021-06-30 12:51:06 +0530

    Completed THA-10 Memory Game

--- Frontend/THA_10/README.md
+++ Frontend/THA_10/README.md
@@ -1,5 +1,5 @@
 ## Memory Game
 
-<!-- ![Memory Game](.seat_booking.gif) -->
+![Memory Game](.seat_booking.gif)
 
-Visit here to have some fun : [Memory Game](https://audarya07.github.io/devsnest/Frontend/THA_10/)
+Visit here to have play : [Memory Game](https://audarya07.github.io/devsnest/Frontend/THA_10/)
--- Frontend/THA_10/index.js
+++ Frontend/THA_10/index.js
@@ -1,44 +1,96 @@
-// let images = ["./images/guido.jpeg", "./images/dennis.jpeg", "./images/bjarnes.jpeg", "./images/james.jpeg", "./images/brendan.jpeg", "./images/yukihiro.jpeg", "./images/larry.jpeg", "./images/rasmus.jpeg"]
+let images = ["./images/guido.jpeg", "./images/dennis.jpeg", "./images/bjarnes.jpeg", "./images/james.jpeg", "./images/brendan.jpeg", "./images/yukihiro.jpeg"]
 
-
-let images = ["Audi", "Pranjal", "Rajat", "Venkya", "Rohan", "Kunal", "Saurabh", "Tanmay"]
 
 images = [...images, ...images]
 
-for(let i = 0; i < 16; i++){
-    let card = document.createElement("div");
+function createOneCard(idx) {
+    var cardCase = document.createElement("div");
+    cardCase.classList.add("cardCase");
+
+    var card = document.createElement("div");
     card.classList.add("card");
 
-    let front = document.createElement("div");
+    var front = document.createElement("div");
     front.classList.add("front");
 
-    let back = document.createElement("div");
+    var back = document.createElement("div");
+    back.classList.add("back");
 
-    let rand = Math.floor(Math.random()*(16-i));
-    // back.innerHTML = "<img src='"+images[rand]+"'/>"
-    back.innerHTML = "<h2>" + images[rand] + "</h2>"
+    var img = document.createElement("img");
+    img.src = images[idx];
+    img.classList.add("myImg");
 
+    back.appendChild(img);
     card.appendChild(front);
     card.appendChild(back);
+    cardCase.appendChild(card);
+    return cardCase;
+  }
 
-    document.getElementById("cards").appendChild(card);
-    back.classList.add("back");
+var grid = document.querySelector(".grid");
+for (let i = 0; i < 12; i++) {
+    let idx = Math.floor(Math.random() * (12 - i));
+    grid.appendChild(createOneCard(idx));
+    images.splice(idx, 1);
 }
 
-let clicked;
-let chances = 28;
-let cards = document.getElementsByClassName("card");
+let chances = 14;
+let pair = -1;
+let matched = [];
+let correct = 0;
+let unmatched = [];
 
-for(let card of cards){
-    card.addEventListener("click", (b) => {
+var cards = document.querySelectorAll(".card");
+document.querySelector(".stats").innerText = "Moves Left :" + chances;
+
+for (let i = 0; i < 12; i++) {
+  cards[i].addEventListener("click", () => {
+    cards[i].classList.toggle("flipped");
+
+    if (pair != -1 && i != pair) {      // 1st click done and 2nd click not on same card 
         chances--;
-        document.querySelector("span").innerHTML = chances;
-        if(clicked){
-            setTimeout(() => {
-                if(card.lastChild.innerHTML != clicked.lastChild.innerHTML){
-
-                }
-            }, 500);
+        document.querySelector(".stats").innerText = "Moves Left :" + chances;
+      
+        if(cards[i].lastChild.firstChild.src === cards[pair].lastChild.firstChild.src){
+            matched.push(i);
+            matched.push(pair);
+            correct++;
+        } else {
+            unmatched.push(i);
+            unmatched.push(pair);
         }
-    })
+        pair = -1;   // reset pair variable for next iteration
+    } else {        // first click
+        pair = i;
+    }
+  });
+}
+
+// whenever no match -> flip both cards -> this check is done every 1 second
+setInterval(() => {
+    // unmatched will always have 2 cards
+    if (unmatched.length > 0) {
+        for (let i = 0; i < unmatched.length; i++) {
+        cards[unmatched[i]].classList.toggle("flipped");
+        cards[unmatched[i + 1]].classList.toggle("flipped");
+        unmatched.splice(0, 2);
+        }
+    }
+}, 1000);
+
+// check game end condition
+setInterval(() => {
+  if (chances == 0 && correct != 6) {
+    alert("LOST !!");
+    reset();
+  } else if (correct == 6) {
+    alert("WON !!");
+    reset();
+  }
+}, 100);
+
+function reset() {
+    correct = 0;
+    chances = 14;
+    location.reload();
 }
